c578fd96b60efc34f819b41c725981038fe710ec
feelsever <philipp.bhandari@gmail.com>
2019-07-04 14:11:17 +0300

    derive genres, add genres class, add some decor

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from music_parser import parser
+
+
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 class MyForm(FlaskForm):
@@ -11,45 +16,45 @@ class MyForm(FlaskForm):
 
 app = Flask(__name__)
 app.secret_key = b'\xb6V\x07sx\xbc8=^\xd1-\xa8\x9c\xd0"H'
-
+app.register_error_handler(404, page_not_found)
 bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return render_template('content.html')
+    return render_template('index.html', url=url_for('music'))
 
 
-@app.route('/hello/')
-def stranger():
+@app.route('/music/')
+def music():
     form = MyForm()
-    return render_template('temp.html', name='странник', form=form)
-
-
-@app.route('/hello/<name>')
-def hello(name):
-    form = MyForm()
-    return render_template('temp.html', name=name, form=form)
+    return render_template('search.html', name='странник', form=form, url=url_for('submit'))
 
 
 @app.route('/submit', methods=('GET', 'POST'))
 def submit():
     form = MyForm()
     if form.validate_on_submit():
-        return redirect('/success') #обработка формы <---------------------------------------------------------------
-    return render_template('submit.html', form=form)
+        return redirect(f'/show/{form.name.data}')
+    return render_template('search.html', form=form)
 
 
+@app.route('/show/', methods=('GET', 'POST'))
+def show():
+    form = MyForm()
+    return render_template('search.html', name='странник', form=form, url=url_for('submit'))
 
 
-
-
+@app.route('/show/<name>', methods=('GET', 'POST'))
+def show_name(name):
+    result = parser.main(parser.g_object, name)
+    return render_template('result.html', artists=result[0], genres=result[1])
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)  # Опция debug включает перезагрузку сервера при изменении кода.
 
-
+url_for()
 
 
 
---
+++ music_parser/parser.py
@@ -0,0 +1,128 @@
+import logging
+from grab import Grab
+from bs4 import BeautifulSoup
+import urllib.parse as url_parse
+from flask import abort
+
+logger = logging.getLogger('grab')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
+g_object = Grab()
+g_object.setup(log_dir='log')
+web_url = 'https://music.yandex.ru'
+
+
+class Artist:
+    def __init__(self, name, href, genres):
+        self.name = name
+        self.genres = genres
+        self.href = href
+
+    def __str__(self):
+        artist_string = f'\nИмя исполнителя:\n\t{self.name}\nЖанры:\n'
+        for genre in self.genres:
+            artist_string += f'\t{genre}'
+        return artist_string
+
+
+class Genre:
+    def __init__(self, name, href):
+        self.name = name
+        self.href = href
+
+    def __str__(self):
+        return self.name
+
+
+def create_artist_list(elems):
+    artist_list = []
+
+    for artist in elems:
+        genres_list = []
+        link = artist.find('a', 'd-link', 'deco-link')
+        name = link.getText()
+
+        link_href = url_parse.urljoin(web_url, link['href'])
+
+        try:
+            genres = artist.find('div', 'd-genres').find_all('a')
+            for genre in genres:
+                genre_name = genre.getText()
+                genre_href = url_parse.urljoin(web_url, genre['href'])
+                genres_list.append(Genre(genre_name, genre_href))
+        except:
+            genre_name = 'Неизвестно'
+            genre_href = '#'
+            genres_list.append(Genre(genre_name, genre_href))
+
+        new_artist = Artist(name, link_href, genres_list)
+        artist_list.append(new_artist)
+    return artist_list
+
+
+def collect_genres(artist_list):
+    genres_names = []
+    genres_list = []
+    for artist in artist_list:
+        for genre in artist.genres:
+            if genre.name not in genres_names:
+                genres_list.append(genre)
+                genres_names.append(genre.name)
+    return genres_list
+
+
+def main(g, name):
+    g.go(f'{web_url}/users/{name}/artists')
+    resp = g.doc
+
+    if resp.code == 404:
+        abort(404)
+
+    if not 200 <= resp.code <= 400:
+        raise Exception(f'response not valid: {resp.code}')
+
+    result = resp.unicode_body()
+    soup = BeautifulSoup(result, 'lxml')
+    artists_html = soup.find_all('div', 'artist')
+
+    artists = create_artist_list(artists_html)
+    genres_list = collect_genres(artists)
+
+    return [artists, genres_list]
+
+
+if __name__ == '__main__':
+    main(g_object, 'filipp.pravda')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
